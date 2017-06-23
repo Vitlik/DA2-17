aedef3d2f5f501d3e7b5934c6fb32b121b9b07ce
Tac713 <TTobollik@gmail.com>
2017-06-23 15:46:44 +0200

    Changed comments in rgbNorm.

--- R/normalization.R
+++ R/normalization.R
@@ -10,12 +10,9 @@ rgbNorm <- function(filename, x = 480, y = 640, z = 3){
   
   normImg <- array(seq(0,0), dim = c(x,y,z))
   # Compute the normalized RGB values by dividing each pixel's value in each dimension by S
-  #normImg[,,1] <- img[,,1]/S
-  #normImg[,,2] <- img[,,2]/S
-  #normImg[,,3] <- img[,,3]/S
   normV <- sapply(1:z,function(x) img[,,x]/S)
   normImg <- array(normV, dim = c(x,y,z))
-  # Set NaN values, received from dividing 0 by something, to 0
+  # Set NaN values, received from dividing 0 by 0, to 0
   normImg[is.nan(normImg)] = 0
   
   writePNG(normImg,paste(filename2,"Norm.png", sep = ""))
