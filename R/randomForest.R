4c117b239c5aa5b3f8b59c9b8035bb1d26128d78
Tac713 <TTobollik@gmail.com>
2017-06-26 23:09:25 +0200

    Added rough parallelized randomForest algorithm.

--- R/randomForest.R
+++ R/randomForest.R
@@ -11,15 +11,23 @@
 #'   \item \code{\link{d.c.step2}}
 #' }
 #'
-#' @author Vitali Friesen
+#' @author Vitali Friesen, Colin Juers, Tassilo Tobollik
 d.a.randomForest.start <- function(){
-  library(randomForest)
+  library(raster)
+  library(snow)
+  library(caret)
+  
+  #Loop through the train/test-data-sets
+  
+  
+  sapply(1:numSets, function(x){
+    # Explanation
+    d.b.step1(x)
+    
+    # Evaluate the result for the train-test-set
+    d.c.step2(x)
+  }
 
-  # Explanation
-  d.b.step1()
-
-  # Explanation
-  d.c.step2()
 }
 
 #' @title Classifier 1 - Step 1
@@ -28,11 +36,42 @@ d.a.randomForest.start <- function(){
 #'
 #' ...
 #'
-#' @author
+#' @author Vitali Friesen, Colin Juers, Tassilo Tobollik
 d.b.step1 <- function(){
   set.seed(1337)
+  
+  #Get or set Train-data and Test-data
+  
+  start.time.train <- Sys.time()
 
-  fore <- randomForest()
+  #Train the randomForest model on the train data
+  parallelRfModel <- train(as.factor(characterVisible) ~ bucketData, 
+               data=train,
+               method = "rf",
+               importance=TRUE,
+               #Parameter-Tuning
+               ntree=2000)
+  
+  #Vllt. auslagern
+  end.time.train <- Sys.time()
+  time.taken.train <- end.time.train - start.time.train
+  time.taken.train
+  
+  #Plot the variable importance of the trained model
+  variableImportance <- varImp(parallelRfModel)
+  plot.varImp.train(variableImportance)
+  
+  start.time.pred <- Sys.time()
+  
+  #Predict the test data on the trained model parallalized
+  beginCluster()
+    preds_rf <- clusterR(testData, raster::predict, args = list(model = parallelRfModel))
+  endCluster()
+  
+  #Vllt. auslagern
+  end.time.pred <- Sys.time()
+  time.taken.pred <- end.time.pred - start.time.pred
+  time.taken.pred
 }
 
 #' @title Classifier 1 - Step 2
@@ -43,5 +82,5 @@ d.b.step1 <- function(){
 #'
 #' @author
 d.c.step2 <- function(){
-
+  #Compare prediction of the Test-Data with the real characterVisible column
 }
