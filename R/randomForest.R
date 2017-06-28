e062474e214569bc6b34776b0046c0433e05db97
Vit <a-commit@vfriesen.de>
2017-06-28 13:04:21 +0200

    run all test blocks on random forest

--- R/randomForest.R
+++ R/randomForest.R
@@ -17,15 +17,34 @@ d.a.randomForest.start <- function(){
   library(snow)
   library(caret)
 
+  load("data/clasAll.rda")
+  load("data/colorHistOriginal.rda")
+  data <- cbind(colorHistOriginal, clasAll[,3])
+
   #Loop through the train/test-data-sets
+  sapply(1:blockNum, function(curBlock){
+    # retrieve the indexes of the corresponding train block
+    trainBlockIndexes <- mget(paste0("train", curBlock), envir=blocks)
 
-
-  sapply(1:numSets, function(x){
+    # for calculating the processing time: save start time
+    start.time <- Sys.time()
     # Explanation
-    d.b.step1(x)
+    d.b.step1(data[trainBlockIndexes,])
+    # print processing time
+    print(paste0("Processing time for training the random forest block ", curBlock, ": ",
+                 (Sys.time() - start.time)))
 
+
+    # retrieve the indexes of the corresponding test block
+    testBlockIndexes <- mget(paste0("test", curBlock), envir=blocks)
+
+    # for calculating the processing time: save start time
+    start.time <- Sys.time()
     # Evaluate the result for the train-test-set
-    d.c.step2(x)
+    d.c.step2(data[testBlockIndexes,])
+    # print processing time
+    print(paste0("Processing time for testing the random forest block ", curBlock, ": ",
+                 (Sys.time() - start.time)))
   })
 
 }
@@ -37,41 +56,21 @@ d.a.randomForest.start <- function(){
 #' ...
 #'
 #' @author Vitali Friesen, Colin Juers, Tassilo Tobollik
-d.b.step1 <- function(){
+d.b.step1 <- function(trainData){
   set.seed(1337)
 
-  #Get or set Train-data and Test-data
-
-  start.time.train <- Sys.time()
 
   #Train the randomForest model on the train data
   parallelRfModel <- train(as.factor(characterVisible) ~ bucketData,
-               data=train,
+               data=trainData,
                method = "rf",
                importance=TRUE,
                #Parameter-Tuning
                ntree=2000)
 
-  #Vllt. auslagern
-  end.time.train <- Sys.time()
-  time.taken.train <- end.time.train - start.time.train
-  time.taken.train
-
   #Plot the variable importance of the trained model
   variableImportance <- varImp(parallelRfModel)
   plot.varImp.train(variableImportance)
-
-  start.time.pred <- Sys.time()
-
-  #Predict the test data on the trained model parallalized
-  beginCluster()
-    preds_rf <- clusterR(testData, raster::predict, args = list(model = parallelRfModel))
-  endCluster()
-
-  #Vllt. auslagern
-  end.time.pred <- Sys.time()
-  time.taken.pred <- end.time.pred - start.time.pred
-  time.taken.pred
 }
 
 #' @title Classifier 1 - Step 2
@@ -80,7 +79,13 @@ d.b.step1 <- function(){
 #'
 #' ...
 #'
-#' @author
-d.c.step2 <- function(){
+#' @author Vitali Friesen, Colin Juers, Tassilo Tobollik
+d.c.step2 <- function(testData){
   #Compare prediction of the Test-Data with the real characterVisible column
+
+
+  #Predict the test data on the trained model parallalized
+  beginCluster()
+  preds_rf <- clusterR(testData, raster::predict, args = list(model = parallelRfModel))
+  endCluster()
 }
