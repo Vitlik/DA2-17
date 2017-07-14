e53e577b289ee032214517e0c725d6d4ef64bb20
Vit <a-commit@vfriesen.de>
2017-07-14 13:44:57 +0200

    minor update

--- R/randomForest.R
+++ R/randomForest.R
@@ -104,6 +104,7 @@ d.a.randomForest.start <- function(a, b, c, d, numTrees){
   save(blocks, file = c)
 
   result <- d.d.evaluation(overallResult[,1], overallResult[,2])
+  print(result)
   
   # d.e.plotImportanceColorHist(blocks)
   
