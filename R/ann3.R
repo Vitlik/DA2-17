b2c0988e77778b9f30f3c1cdafa6134b05897b5c
Nils <nmeckmann@gmail.com>
2017-07-16 11:37:58 +0200

    Changes to ann

--- R/ann3.R
+++ R/ann3.R
@@ -17,113 +17,110 @@ setwd("C:/dev/DA2/DA2-17/")
 
 load("data/blocks2677IMG.rda")
 load("data/classesOrig.rda")
-load("data/colorHistOriginal255Buckets.rda")
-load("data/hog_original_10_6_complete.rda")
+load("data/colorHistEighthRGBNorm16Buckets.rda")
+load("data/hog_original_15_6_complete.rda")
+load("data/colorHistEighth255Buckets.rda")
+load("data/hog_eighth__complete.rda")
+load("data/classesEights.rda")
+load("data/hog_eighth_8_9_complete.rda")
 
-data1 <- cbind(hogData, colorHist, P = classesOrig[, "P"])
+data1 <- cbind(hogData, P = classesEights[, "P"])
 
-##CNNModels <- new.env()
-resultData1 <- sapply(1:blockNum, function(curBlock){
+ann.execute <- function(dataType, imageType, rounds, lr, nodes, batch){
+  ##CNNModels <- new.env()
+  resultData1 <- sapply(1:blockNum, function(curBlock){
+    
+    library(mxnet)
+    
+    # retrieve the indexes of the corresponding train block
+    trainBlockIndexes <- get(paste0("train", curBlock), envir=blocks)
+    
+    # for calculating the processing time: save start time
+    start.time <- Sys.time()
+    
+    # Explanation
+    trainData <- data1[trainBlockIndexes,]
+    # trainData <- data1
+    train_x <- data.matrix(trainData[,-ncol(trainData)])
+    train_y <- trainData[,ncol(trainData)]
+    
+    ANNModel <- ann.step1(train_x, train_y)
+    
+    assign(paste0("annModel", curBlock), ANNModel, envir = blocks)
+    
+    ##assign(paste0("CNNModel", curBlock), CNNModel, envir = CNNModels)
+    # print processing time
+    print(paste0("Processing time for training the ANN block ", curBlock, ": ",
+                 (Sys.time() - start.time)))
+    
+    # retrieve the indexes of the corresponding test block
+    testBlockIndexes <- get(paste0("test", curBlock), envir=blocks)
+    
+    # for calculating the processing time: save start time
+    start.time <- Sys.time()
+    testData <- data1[testBlockIndexes,]
+    
+    test_x <- data.matrix(testData[,-ncol(testData)])
+    test_y <- testData[,ncol(testData)]
+    
+    # Evaluate the result for the train-test-set
+    preds <- predict(ANNModel, test_x)
+    
+    colnames(preds) <- rownames(test_x)
+    rownames(preds) <- c(0,1)
+    #View(tPreds)
+    
+    preds <- t(preds)
+    #View(tPreds2)
+    
+    assign(paste0("predsWithProbs", curBlock), preds, envir = blocks)
+    
+    predsValues <- as.numeric(colnames(preds)[max.col(preds, ties.method = 'first')])
+    
+    #preds <- ann.step2(test.x, ANNModel)
+    # print processing time
+    print(paste0("Processing time for testing the ANN block ", curBlock, ": ",
+                 (Sys.time() - start.time)))
+    
+    # print(pred.label <- max.col(t(preds)) - 1)
+    # print(table(pred.label))
+    # 
+    # print(table(test_y, pred.label))
+    # print(sum(diag(table(test_y, pred.label)))/530)
+    
+    #result = data.frame(cbind(predsValues, test_y))
+    
+    result <- matrix(nrow = length(predsValues), ncol = 2)
+    result[,1] <- as.vector(predsValues)
+    result[,2] <- as.vector(test_y)
+    return(result)
+  })
   
-  # retrieve the indexes of the corresponding train block
-  trainBlockIndexes <- get(paste0("train", curBlock), envir=blocks)
+  overallResult <- do.call(rbind, resultData1)
   
-  # for calculating the processing time: save start time
-  start.time <- Sys.time()
+  assign(paste0("overallResult"), overallResult, envir = blocks)
   
-  # Explanation
-  trainData <- data1[trainBlockIndexes,]
-  # trainData <- data1
-  train_x <- data.matrix(trainData[,-ncol(trainData)])
-  train_y <- trainData[,ncol(trainData)]
+  fileName = paste(dataType, imageType, rounds, lr, nodes, batch, sep="_")
+  filePath = paste("data/", fileName, ".rda")
+  
+  save(blocks, file = filePath)
+  #save(blocks, file = "data/hog_5_6_eighth_rounds30_lr0_0007_nodes1500_batch50.rda")
+  
+  d.d.evaluation(overallResult[,1], overallResult[,2])
+}
 
-  ANNModel <- ann.step1(train_x, train_y)
-  
-  assign(paste0("annModel", curBlock), ANNModel, envir = blocks)
-  
-  ##assign(paste0("CNNModel", curBlock), CNNModel, envir = CNNModels)
-  # print processing time
-  print(paste0("Processing time for training the ANN block ", curBlock, ": ",
-               (Sys.time() - start.time)))
-  
-  # retrieve the indexes of the corresponding test block
-  testBlockIndexes <- get(paste0("test", curBlock), envir=blocks)
-  
-  # for calculating the processing time: save start time
-  start.time <- Sys.time()
-  testData <- data1[testBlockIndexes,]
-  
-  test_x <- data.matrix(testData[,-ncol(testData)])
-  test_y <- testData[,ncol(testData)]
+ann.execute("hog_8_9", "eighth", "50", "0.00001", "1500", "20")
 
-  # Evaluate the result for the train-test-set
-  preds <- predict(ANNModel, test_x)
-  
-  colnames(preds) <- rownames(test_x)
-  rownames(preds) <- c(0,1)
-  #View(tPreds)
-  
-  preds <- t(preds)
-  #View(tPreds2)
-  
-  assign(paste0("predsWithProbs", curBlock), preds, envir = blocks)
-  
-  predsValues <- as.numeric(colnames(preds)[max.col(preds, ties.method = 'first')])
-
-  #preds <- ann.step2(test.x, ANNModel)
-  # print processing time
-  print(paste0("Processing time for testing the ANN block ", curBlock, ": ",
-               (Sys.time() - start.time)))
-  
-  # print(pred.label <- max.col(t(preds)) - 1)
-  # print(table(pred.label))
-  # 
-  # print(table(test_y, pred.label))
-  # print(sum(diag(table(test_y, pred.label)))/530)
-  
-  #result = data.frame(cbind(predsValues, test_y))
-  
-  result <- matrix(nrow = length(predsValues), ncol = 2)
-  result[,1] <- as.vector(predsValues)
-  result[,2] <- as.vector(test_y)
-  return(result)
-})
-
-overallResult <- do.call(rbind, resultData1)
-
-assign(paste0("overallResult"), overallResult, envir = blocks)
-
-save(blocks, file = "data/hog_10_6_histOrig_original_rounds40_lr0_000001_nodes2000_batch20.rda")
-
-d.d.evaluation(overallResult[,1], overallResult[,2])
 
 ann.step1 <- function(train_array, train_y){
   library(mxnet)
   mx.set.seed(1)
   
   # Model
-  ANNModel <- mx.mlp(train_array, train_y, hidden_node=2000, out_node=2, out_activation="softmax",
-                 num.round=40, array.batch.size=20, learning.rate=0.000001, momentum=0.9,
+  ANNModel <- mx.mlp(train_array, train_y, hidden_node=1500, out_node=2, out_activation="softmax",
+                 num.round=50, array.batch.size=20, learning.rate=0.00001, momentum=0.9,
                  eval.metric=mx.metric.accuracy)
   return(ANNModel)
-}
-
-ann.step2 <- function(test_array, ANNModel){
-  library(mxnet)
-  
-  preds <- predict(ANNModel, test_array)
-  
-  rownames(tPreds) <- c(0,1)
-  #View(tPreds)
-  
-  tPreds2 <- t(tPreds)
-  #View(tPreds2)
-  
-  colnames(tPreds2)
-  
-  preds <- colnames(tPreds2)[max.col(tPreds2, ties.method = 'first')]
-  
-  return(preds)
 }
 
 
