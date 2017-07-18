29ce86b4dddec3de98f6cccacca1fc5a334053bf
Tac713 <TTobollik@gmail.com>
2017-07-18 14:48:06 +0200

    Moved a methode from normalization to preprocessing and added roxygen annotations for it.

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -344,3 +344,34 @@ b.j.assignSimpelAccuracy <- function(image){
   d.d.evaluation(pred, classesEights[, "P"])
 }
 
+#' @title Preprocessing - 
+#' @description Set \code{setwd()} to the location of the image.
+#' To get (back) to the overview of all steps and functions use this link:
+#' \code{\link{a.a.main}}
+#' @param filename A string that holds the filename
+#' @param x A number that describes the height of the image in pixels
+#' @param y A number that describes the width of the image in pixels
+#' @param z A number that describes the number of colour dimensions of the image
+#' @author Tassilo Tobollik
+#Takes approx. 1.1294 secs per screenshot
+b.k.rgbNorm <- function(filename, x = 480, y = 640, z = 3){
+  pos <- gregexpr(".png", filename)
+  filename2 <- substr(filename, 1, (pos[[1]]-1))
+  
+  img = readPNG(filename)
+  
+  # Compute the sums of the pixel's values over all channels
+  S <- apply(img,c(1,2),sum)
+  
+  normImg <- array(seq(0,0), dim = c(x,y,z))
+  # Compute the normalized RGB values by dividing each pixel's value in each dimension by S
+  normV <- sapply(1:z,function(x) img[,,x]/S)
+  normImg <- array(normV, dim = c(x,y,z))
+  # Set NaN values, received from dividing 0 by 0, to 0
+  normImg[is.nan(normImg)] = 0
+  
+  writePNG(normImg,paste(filename2,"Norm.png", sep = ""))
+  return(normImg)
+}
+
+
